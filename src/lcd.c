c1aa9f4e465b6f10b1230d14f56e88b03fa018c0
Passy1977 <passy.linux@zresa.it>
2022-09-05 23:30:32 +0200

    feat: rename gpio pin

--- src/lcd.c
+++ src/lcd.c
@@ -17,6 +17,62 @@
 
 
 #include "lcd.h"
+#include "gpio_config.h"
 
-const __u8 HDG_LCD_WIDTH = 16;
-const __u8 HDG_LCD_HEIGHT = 2;
+#include <linux/delay.h>
+
+
+static void hgd_lcd_pulse(void);
+static void hgd_lcd_write(__u8 value);
+static void hgd_lcd_send(__u8 value);
+
+/**
+ * @brief Init lcd
+ * 
+ */
+bool hgd_lcd_init(hgd_error_t** error)
+{
+    gpio_set_value(HGD_GPIO_LED_RS, false);
+    gpio_set_value(HGD_GPIO_LED_RW, false);
+    gpio_set_value(HGD_GPIO_LED_E, false);
+    gpio_set_value(HGD_GPIO_LED_BL, false);
+    gpio_set_value(HGD_GPIO_LED_D4, false);
+    gpio_set_value(HGD_GPIO_LED_D5, false);
+    gpio_set_value(HGD_GPIO_LED_D6, false);
+    gpio_set_value(HGD_GPIO_LED_D7, false);
+
+//hgd_lcd_send('c');
+    usleep_range(40000, 2000);
+
+    gpio_set_value(HGD_GPIO_LED_D4, true);
+    gpio_set_value(HGD_GPIO_LED_D5, true);
+
+    return true;
+}
+
+
+void hgd_lcd_pulse(void)
+{
+    gpio_set_value(HGD_GPIO_LED_E, false);
+    usleep_range(1, 10);
+    gpio_set_value(HGD_GPIO_LED_E, true);
+    usleep_range(1, 10);
+    gpio_set_value(HGD_GPIO_LED_E, false);
+    usleep_range(100, 200);
+}
+
+void hgd_lcd_write(__u8 value)
+{
+    gpio_set_value(HGD_GPIO_LED_D4, (value >> 0 ) & 0x01);
+    gpio_set_value(HGD_GPIO_LED_D5, (value >> 1 ) & 0x01);
+    gpio_set_value(HGD_GPIO_LED_D6, (value >> 3 ) & 0x01);
+    gpio_set_value(HGD_GPIO_LED_D7, (value >> 4 ) & 0x01);
+
+    hgd_lcd_pulse();
+}
+
+static void hgd_lcd_send(__u8 value)
+{
+    hgd_lcd_write(value >> 0x04);
+    hgd_lcd_write(value);
+}
