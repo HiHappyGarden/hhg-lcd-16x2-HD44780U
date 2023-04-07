cf8286d3d6c51591917345d13a2f6ab7c9424f59
Antonio Salsi <passy.linux@zresa.it>
2023-04-07 13:47:56 +0100

    update copyright

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Happy GardenPI distribution (https://github.com/HappyGardenPI/happy-gardenpi-driver).
- * Copyright (c) 2022 Antonio Salsi.
+ * Copyright (c) 2022-23 Antonio Salsi.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
